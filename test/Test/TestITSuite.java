28d325319b10062a968795d7d15ad87583d330d8
41586220 <41586220@087921-P33S103.macklabs.br>
2016-09-14 09:30:27 -0300

    ticketMachine

---
+++ test/Test/TestITSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author 31210856
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Test.TesteIT.class})
+public class TestITSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
