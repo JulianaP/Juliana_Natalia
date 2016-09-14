e015c264da541c9e3bf750bb22ff33844f5a9a13
41586220 <41586220@087921-P33S103.macklabs.br>
2016-09-14 09:29:16 -0300

    ticketMachine

---
+++ src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -0,0 +1,36 @@
+package br.calebe.ticketmachine.core;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoeda {
+
+    protected int valor;
+    protected int quantidade;
+    
+    public PapelMoeda(){
+        
+    }
+
+    public PapelMoeda(int valor, int quantidade) {
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
